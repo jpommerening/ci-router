ad4de9e42eb2011aadc3b2a5aab52c3694c6c6c2
Jonas Pommerening <jonas.pommerening@gmail.com>
2015-12-04 13:51:52 +0100

    Make the default export more accessible in nodejs

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import { Router } from './router';
 export default function router(adapter, options) {
   return new Router(adapter, options);
 }
+
+// Make router() available as require('router')
+Object.assign(router, exports);
+module.exports = router;
